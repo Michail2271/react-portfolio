20f7bb4156dd77c5c4e1f4d16f0a0b6f2037191d
Michail2271 <misha2271@icloud.com>
2019-11-15 20:06:07 +0300

    init

---
+++ src/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Main.module.css';
+
+
+function Main() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.greeting}>
+          <span>Привет</span>
+          <span>Меня зовут Михаил Марчук</span>
+          <span>Я front-end разработчик</span>
+        </div>
+        <div className={styles.photo}>
+        <img src=''/> 
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Main;
