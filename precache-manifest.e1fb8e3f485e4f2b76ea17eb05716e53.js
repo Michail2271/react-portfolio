3cb848d6351221c1009dc27b254e7f76a90a7f8d
Michail2271 <misha2271@icloud.com>
2019-11-23 20:36:35 +0300

    Updates

---
+++ precache-manifest.e1fb8e3f485e4f2b76ea17eb05716e53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81682cd60fcbd491e210f3fd34a9329b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d1b36480f41ec95ce662",
+    "url": "/react-portfolio/static/css/main.88d0ea2f.chunk.css"
+  },
+  {
+    "revision": "26fcd55308d6e66e64cd",
+    "url": "/react-portfolio/static/js/2.eb6da08f.chunk.js"
+  },
+  {
+    "revision": "d1b36480f41ec95ce662",
+    "url": "/react-portfolio/static/js/main.ab9c6e73.chunk.js"
+  },
+  {
+    "revision": "7b82ede9c0ee1b7e876e",
+    "url": "/react-portfolio/static/js/runtime-main.4d1b885d.js"
+  }
+]);
